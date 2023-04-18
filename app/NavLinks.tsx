172c29f8ea392dd59252b8274ae476ed18ae3dc6
Supakorn <supakorn0728@gmail.com>
2023-04-18 23:32:11 +0700

    update

--- app/NavLinks.tsx
+++ app/NavLinks.tsx
@@ -11,7 +11,7 @@ function NavLinks() {
     };
 
     return (
-        <nav>
+        <nav className="grid grid-cols-4 md:grid-cols-7 text-xs md:text-sm gap-4 pb-10 max-w-6xl mx-auto border-b">
             {categories.map((category) => (
                 <NavLink key={category} category={category} isActive={isActive(category)} />
             ))}
