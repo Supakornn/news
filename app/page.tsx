03f0cc6b18b7cd627ea37128752f238787db4fc0
Supakorn <supakorn0728@gmail.com>
2023-05-01 05:51:14 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import { categories } from "../constants";
 import fetchNews from "../lib/fetchNews";
 import NewsList from "./NewsList";
-import response from "../response.json";
 
 async function Homepage() {
     const news: NewsResponse = await fetchNews(categories.join(","));
-    console.log(news);
     return (
         <div>
             <NewsList news={news} />
